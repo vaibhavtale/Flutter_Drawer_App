ce9c35ea45beed5b6046773770df7a07484fd008
Vaibhav Tale <vaibhavtale20@gmail.com>
2023-02-08 18:19:53 +0530

    Flutter Drawer App Day1

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.lime,
+        ),
+        home: HomePage(),
+      )
+  );
+}
+
